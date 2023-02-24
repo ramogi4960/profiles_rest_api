474f1a57561978baf1200ec674c0318a1b85b52b
Kevin <kramzyvine@icloud.com>
2023-02-24 13:43:48 +0300

    Pimped the creation of super_user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -18,9 +18,11 @@ class UserProfileManager(BaseUserManager): #manager for user profiles
         return user
 
     def create_superuser(self, email, name, password):
+        ''' create and save new super user with given details '''
         user = self.create_user(email, name, password)
-        user.is_superuser=True
-        user.is_staff=True
+
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
