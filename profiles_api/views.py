5f23815d2c64cf3af3a416b22b5f682c872c8a73
Kevin <kramzyvine@icloud.com>
2023-02-26 23:25:38 +0300

    Added GET,POST,PUT,PATCH and DELETE methods into API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
-        """Returns a list of API views feaatures"""
+        """Returns a list of API views features"""
         an_apiview = [
             'uses HTTP methods as functions(get, post, put, patch, delete)',
             'I similar to a traditional django view',
@@ -13,4 +17,30 @@ class HelloApiView(APIView):
             'Is mapped manually to url'
         ]
         
-        return Response({'Message':'Hello !\n\n', 'APIViews features': an_apiview})
+        return Response({'Message': 'Hello !', 'APIViews features': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our API"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'Message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """ Handle updating an object """
+        return Response({'Message': 'Put'})
+
+    def patch(self, request, pk=None):
+        """ Handle partial update of object """
+        return Response({'Message': 'Patch'})
+
+    def delete(self, request, pk=None):
+        """ Deletes an object """
+        return Response({'Method': 'Delete'})
