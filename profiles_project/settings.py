fc2907c441cc98445b76050c68165b1d42a12708
Kevin <kramzyvine@icloud.com>
2023-03-10 15:15:59 +0300

    Configured for server deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3lo5ry9ojcdbgsy5-5-gyk((7x$i0jefrqec*s=_x2(o#w8s&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -117,3 +117,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
